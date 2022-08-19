6e6b8d1e6eb16f76ca55c81eb487e7188cefceeb
AlCalzone <d.griesel@gmx.net>
2022-08-19 16:09:32 +0200

    feat: convert to hybrid package (#1)

---
+++ .github/test-scripts/test.cjs
@@ -0,0 +1,4 @@
+const isPathCwd = require("@esm2cjs/is-path-cwd").default;
+const assert = require("assert");
+
+assert(typeof isPathCwd === "function");
---
+++ .github/test-scripts/test.mjs
@@ -0,0 +1,4 @@
+import isPathCwd from "@esm2cjs/is-path-cwd";
+import assert from "assert";
+
+assert(typeof isPathCwd === "function");
